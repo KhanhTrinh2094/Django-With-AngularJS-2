a2577dfdcdc27288899049cf53c1256ca3230012
Trinh.NCK <trinh.nck@gmail.com>
2017-03-13 04:33:11 +0700

    Add authentication

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,6 +2,8 @@ from rest_framework_nested import routers
 
 from authentication.views import AccountViewSet
 
+from authentication.views import LoginView
+
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
 
@@ -9,6 +11,8 @@ urlpatterns = patterns(
      '',
     # ... URLs
     url(r'^api/v1/', include(router.urls)),
+
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
 
     url('^.*$', IndexView.as_view(), name='index'),
 )
